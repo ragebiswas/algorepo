12fcc8f0118d63b110eed2ee59e22693778d81fe
Rajorshi Biswas <ragebiswas@gmail.com>
2013-10-08 22:39:33 +0530

    SPOJ: Triple Fat Ladies

---
+++ spoj/eights.py
@@ -0,0 +1,9 @@
+#
+# Problem: http://www.spoj.com/problems/EIGHTS/
+# Category: Simple math
+#
+nc = int(raw_input())
+while nc != 0:
+    nc -= 1
+    k = int(raw_input())
+    print 192 + (k - 1) * 250
